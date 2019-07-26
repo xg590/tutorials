99a17e78a8ae038188c86ccc428f2ae3f5c3fb5c
xg590 <43154552+xg590@users.noreply.github.com>
2019-07-26 16:27:46 -0400

    Update backend.py

--- web_dev/backend.py
+++ web_dev/backend.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template, jsonify
-from flask_cors import CORS
+from flask import Flask, render_template, jsonify 
 from random import *
 app = Flask(__name__, template_folder = "./") # if you would like place frontend.html in another folder,
-                                              # change the template_folder
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+                                              # change the template_folder 
 @app.route('/api/random')
 def random_number():
     response = {
         'randomNumber': randint(1, 100)
     }
     return jsonify(response)
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
+@app.route('/'）
+def frontpage():
     return render_template("frontend.html")
 app.run('127.0.0.1', 5000)
 
